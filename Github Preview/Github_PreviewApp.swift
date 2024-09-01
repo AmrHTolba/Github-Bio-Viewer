89403d740363b30344672bb90f54187437665a78
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-09-01 22:31:27 +0300

    Initial Commit

---
+++ Github Preview/Github_PreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_PreviewApp.swift
+//  Github Preview
+//
+//  Created by Amr El-Fiqi on 01/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Github_PreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
